39b4695d55d9a1105aea6b2d0d3efdb30ceae454
Luqman Rasat <luqmanrasat@hotmail.com>
2022-07-16 14:19:35 +0800

    add db & user entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { CustomersModule } from './customers/customers.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [CustomersModule, UsersModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'pass123',
+      database: 'tutorial_db',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    CustomersModule,
+    UsersModule,
+  ],
   controllers: [],
   providers: [],
 })
